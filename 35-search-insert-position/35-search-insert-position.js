7b73e094b8e306734c8b56165ea42a4140d6d068
BahaAbuelwan <100941589+BahaAbuelwan@users.noreply.github.com>
2022-07-12 23:31:17 +0300

    Time: 62 ms (93.78%), Space: 41.8 MB (88.51%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(target>nums[nums.length-1]) return nums.length
+  let start = 0,
+    end = nums.length - 1,
+    answer = -1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+      answer=mid
+    }
+    
+   
+  }
+
+  return answer
+};
