7f70e5f8cd9730e18656f5ad87191e14be15b702
BahaAbuelwan <100941589+BahaAbuelwan@users.noreply.github.com>
2022-07-15 15:25:06 +0300

    Time: 131 ms (20.74%), Space: 42.5 MB (94.62%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+      let result = 0;
+    
+    for(let i=0; i<columnTitle.length; i++) {
+        
+        let char = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1;
+        
+        result = result * 26 + char;
+    }
+    
+    return result;
+
+};
