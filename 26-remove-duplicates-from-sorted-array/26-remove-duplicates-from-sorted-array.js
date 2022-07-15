72e34c12771127ada8fe9047a7f61cbb0a9e16b7
BahaAbuelwan <100941589+BahaAbuelwan@users.noreply.github.com>
2022-07-15 14:12:11 +0300

    Time: 205 ms (14.61%), Space: 45.3 MB (13.40%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -3,19 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let arr=[]
-    let count=  0
-    for(let i=0; i<nums.length;i++){
-        if(!arr.includes(nums[i])){
-            arr.push(nums[i])
-            
-        }
-        else{
-            console.log(i)
-            nums.splice(i,1)
-            i--
-        }
+     let obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (obj[nums[i]] === undefined) {
+      obj[nums[i]] = 1;
+    } else {
+      nums.splice(i, 1);
+      i--;
     }
-    console.log(nums)
-    return 
+  }
+
+  return;
 };
